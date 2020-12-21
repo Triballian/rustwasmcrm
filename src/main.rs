9f3758658e8e204677c44d2899db69f655d5efb4
RustDot <rust.coach@gmail.com>
2020-12-21 12:59:18 -0600

    added type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+select customer {
+    id: String,
+    firstname: String,
+    secondname: String,
+    number: String,
+    address: String,
+    ebill: ebillrange,
+    note: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
