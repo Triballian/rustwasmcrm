ac563197dd5f12d4c285602456444959ba3ba2ed
RustDot <rust.coach@gmail.com>
2020-12-28 09:21:42 -0600

    added new Customer A function

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,30 @@
-use chrono::{Datelike, Local, Utc};
+use chrono::prelude::*;
 
 struct Customer {
   id: String,
-  address: String,
+  address: Address,
   firstname: String,
   secondname: String,
   phonenumber: String,
   email: String,
-  ebill: Ebillrange,
+  ebill: Vec<String>,
   date_created: DateTime<Utc>,
   notes: String,
   created_by: String,
 }
 impl Customer {
- fn new {
-   
+ fn new(id, address, firstname, secondname, email, phone, ebillrange, date_created, created_by) {
+   Self {
+    id,
+    address,
+    firstname,
+    secondname,
+    email,
+    phone,
+    ebillrange,
+    date_created,
+    created_by,
+   }   
  }
 }
 
@@ -33,39 +43,44 @@ impl Address {
    }
  }
 }
-struct Ebillrange {"0-40","50-90","100-140","150-190","200-240","250-290","300-340","350-400"}
+
 
 fn main(){
   
- let street = String::from("564 applebee st");
- let city = String::from("huntsville");
- let zip = String::from("77396");
- let firstname = String::from("Andy");
- let lastname = String::from("Woodsmith");
- let email = String::from("andy.smith@gmail.com");
- let createdby = String::from("Bill Miyer");
- let notes = String::from("Good roof");
  
- let mut id: String;
+let street = String::from("564 applebee st");
+let city = String::from("huntsville");
+let zip = String::from("77396");
+let firstname = String::from("Andy");
+let lastname = String::from("Woodsmith");
+let phone = String::from("225-125-421");
+let email = String::from("andy.smith@gmail.com");
+let ebillrange = vec!["0-40","50-90","100-140","150-190","200-240","250-290","300-340","350-400"];
+
+let createdby = String::from("Bill Miyer");
+let notes = String::from("Good roof");
+ 
+let mut id: String;
 
    loop {
      id = id::generate_alphanumeric(7); // e.g.: 4rf6r7f
-     if self.is_id_available(&id).await {
-       break;
-     }
+     
  
 // Create customer object
    let new_customer = Customer::new(
-     id,
-     customer::Address::new(city, street, zip),
-     firstname,
-     secondname,
-     email,
-     phone,
-     date_created,
-     notes,
-     created_by,
-     date_created: DateTime<Utc>,
+      
+      id,
+      Address::new(city, street, zip),
+      firstname,
+      secondname,
+      email,
+      phone,
+      ebillrange,
+      notes,
+ 
+      date_created: DateTime<Utc>,
+      created_by,
+      
    );
 }
 
